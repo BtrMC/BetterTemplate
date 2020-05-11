f8d5cd8a17ed49ca8fa0f113ea97f63557595da6
razod <masonboulier@gmail.com>
2020-05-10 20:49:13 -0400

    add comments

--- src/main/java/com/your/domain/cmds/helloWorld.java
+++ src/main/java/com/your/domain/cmds/helloWorld.java
@@ -9,6 +9,7 @@ import com.your.domain.Util;
 public class helloWorld implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
         Player p = (Player) sender;
+        // Uses our Util class to send messages quicker and also uses Util.cColor to be able to use & for colors in chat!
         Util.sendPl(p, Util.cColor("&cHello, world!"));
         return true;
     }
